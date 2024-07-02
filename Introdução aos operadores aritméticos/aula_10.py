fab95d9b7c3fe197981ece9ec8a8c31fe894222f
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-02 16:23:55 -0300

    commit

---
+++ Introdução aos operadores aritméticos/aula_10.py
@@ -0,0 +1,8 @@
+# Concatenação (+) e repetição (*) com operadores aritméticos
+concatenacao = 'A' + 'B' + 'C'
+print(concatenacao) # Valores (Tipos) iguais
+
+a_dez_vezes = 'A' * 10
+tres_vezes_beatriz = 3 * 'Beatriz'
+print(a_dez_vezes)
+print(tres_vezes_beatriz)
