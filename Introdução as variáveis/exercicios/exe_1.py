0e122e79e63e7f334255c49c8ba71f58f49f00f9
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-02 12:48:26 -0300

    Exercício 1

---
+++ Introdução as variáveis/exercicios/exe_1.py
@@ -0,0 +1,12 @@
+nome = "Beatriz"
+sobrenome = "Mesquita"
+idade = 22
+ano_nascimento = 2002
+maior_de_idade = (2024 - ano_nascimento) >= 18
+altura_metros = 1.70
+print("Nome: ", nome)
+print("Sobrenome: ", sobrenome)
+print("Idade: ", idade)
+print("Ano de nascimento: ", ano_nascimento)
+print("É maior de idade? ", maior_de_idade)
+print("Altura em metros: ", altura_metros)
