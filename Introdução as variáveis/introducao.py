e1a9af05bd8feea7d8470f1bef10a41833c913e4
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-01 21:53:24 -0300

    alteração

--- Introdução as variáveis/introducao.py
+++ Introdução as variáveis/introducao.py
@@ -1,6 +1,16 @@
 # Variáveis são usadas para salvar algo na memória do computador
 # Uso: nome_variaval = expressão
+# Tente criar nomes descritivos, que deixem contem
 
 nome_completo = "Beatriz Mesquita"
 soma_dois_mais_dois = 2 + 2
-print(nome_completo, soma_dois_mais_dois) # Beatriz Mesquita, 4
+
+print(nome_completo, soma_dois_mais_dois) # Beatriz Mesquita 4
+int_um = int('1') # Evita muitas modificações, basta modificar a "raiz"
+
+print(int_um, type(int_um))
+idade = 22 # Valor inteiro
+maior_de_idade = idade >= 18 # >= Operadores
+
+print('Nome:', nome_completo, 'Idade:', idade) # Nome: Beatriz Mesquita Idade: 22
+print("É maior? ", maior_de_idade) # É maior? True
