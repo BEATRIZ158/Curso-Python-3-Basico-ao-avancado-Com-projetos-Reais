b32aa00ecaf365e32f9eb3026487703a20e50218
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-02 16:12:43 -0300

    adicionando conteudo

--- Introdução aos operadores aritméticos/aula_9.py
+++ Introdução aos operadores aritméticos/aula_9.py
@@ -7,11 +7,20 @@ print("Subtração", subtracao)
 multiplicacao = 10 * 10
 print("Multiplicação", multiplicacao)
 
-divisao = 10 / 2.2
+divisao = 10 / 3 # Divisão normal, sempre retorna número flutuante (float)
 print("Divisão", divisao)
 
-divisao_inteira = 10 // 2.2
+divisao_inteira = 10 // 3 # Retorna um número inteiro, a não ser se dividir por um valor float, corta casas decimais
 print("Divisão Inteira", divisao_inteira)
 
-exponenciacao = 10 // 2.2
+exponenciacao = 2 ** 10 # 2 elevado a 10, cuidado para não extorar a memória do PC
 print("Exponenciação", exponenciacao)
+
+modulo = 25 % 2 # Retorna o resto da divisão
+print("Módulo", modulo)
+
+print(10 % 8 == 0) # Se o resultado for 0, 10 é divisivel por 8,Retorna False ou True
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
+# Testando se é PAR ou IMPAR
