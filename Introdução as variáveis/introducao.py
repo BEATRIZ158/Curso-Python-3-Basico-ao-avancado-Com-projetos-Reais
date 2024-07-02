f855a21f5a5ffd4e2b5bac85083ec5c1a3ca168a
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-01 21:36:02 -0300

    alteração

---
+++ Introdução as variáveis/introducao.py
@@ -0,0 +1,7 @@
+# Variáveis são usadas para salvar algo na memória do computador
+# Uso: nome_variaval = expressão
+
+nome_completo = "Beatriz Mesquita"
+print(nome_completo) # Beatriz Mesquita
+soma_dois_mais_dois = 2 + 2
+print(soma_dois_mais_dois) # 4
