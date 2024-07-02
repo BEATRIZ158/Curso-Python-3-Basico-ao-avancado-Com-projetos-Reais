7e7e0ce9ca6bef3ef14c22f1a5fd1ca3535c508e
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-02 16:03:04 -0300

    alteração

---
+++ Introdução aos operadores aritméticos/aula_9.py
@@ -0,0 +1,17 @@
+adicao = 10 + 10 # Podem ser números inteiros e flutuantes mistos
+print("Adição", adicao)
+
+subtracao = 10 - 5
+print("Subtração", subtracao)
+
+multiplicacao = 10 * 10
+print("Multiplicação", multiplicacao)
+
+divisao = 10 / 2.2
+print("Divisão", divisao)
+
+divisao_inteira = 10 // 2.2
+print("Divisão Inteira", divisao_inteira)
+
+exponenciacao = 10 // 2.2
+print("Exponenciação", exponenciacao)
--- Introdução as variáveis/exercicios/exe_1.py
+++ Introdução as variáveis/exercicios/exe_1.py
@@ -10,3 +10,4 @@ print("Idade: ", idade)
 print("Ano de nascimento: ", ano_nascimento)
 print("É maior de idade? ", maior_de_idade)
 print("Altura em metros: ", altura_metros)
+ 
