5f19cfe4841ef5c0d4442247a34eea214015e62a
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-03 20:18:22 -0300

    imc

---
+++ Introdução aos operadores aritméticos/calculo_IMC_+_elipsis.py
@@ -0,0 +1,8 @@
+nome = 'Beatriz'
+altura = 1.80
+peso = 95
+imc = peso / altura ** 2
+print(nome, 'tem', altura, 'de altura')
+print('pesa', peso, 'quilos e seu imc é', imc)
+
+# ... elipsis, placeholder, não gera erro
