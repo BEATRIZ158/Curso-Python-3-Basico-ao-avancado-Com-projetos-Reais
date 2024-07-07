48a47b92c85be811386d232c13ea32d89b7a802a
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-06 22:28:44 -0300

    aula 16

---
+++ extruturas condicionais/aula_16.py
@@ -0,0 +1,15 @@
+#Introdução aos blocos de código + if / elif / else
+
+# if / elif        / else
+# se / se não se   / se não
+
+entrada = input('Você quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+    print("Voce entrou no sistema!")
+elif entrada == 'sair': # Senão entrou na primeira, tenho outra opção
+    print("Você saiu do sistema")
+else:
+    print("Você digitou uma opção inválida") # Depende do if
+
+print("FORA DOS BLOCOS")
