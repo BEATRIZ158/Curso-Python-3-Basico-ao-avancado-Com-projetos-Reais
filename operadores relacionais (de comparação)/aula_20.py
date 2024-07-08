96f075317c723b47318aae23460eb7cf2ab2d7df
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-07 23:30:01 -0300

    aula 20

---
+++ operadores relacionais (de comparação)/aula_20.py
@@ -0,0 +1,8 @@
+primeiro_valor = input("Digite um valor: ")
+segundo_valor = input("Digite outro valor: ")
+
+if primeiro_valor > segundo_valor:
+    print(f"{primeiro_valor=} é maior que {segundo_valor=}")
+else:
+    print(f"{segundo_valor=} é maior que {primeiro_valor=}")
+    
