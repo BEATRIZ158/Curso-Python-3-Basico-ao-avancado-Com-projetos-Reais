eddec9e899cc7ab621e4043b906afd0255391239
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-11 22:49:25 -0300

    id

---
+++ id, flags, is, is not e None/id.py
@@ -0,0 +1,13 @@
+""" 
+Flag (Bandeira) - Marcar um local
+None = Não valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = Identidade
+
+"""
+
+# As duas variáveis apontam para o mesmo valor na memória
+v1 = 'a'
+v2 = 'a'
+print(id(v1))
+print(id(v2))
