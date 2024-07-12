f654e6ab1d65930e8aaf5f40bc7bc3d5c194bad0
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-11 22:48:23 -0300

    aula 31

---
+++ id, flags, is, is not e None/aula_31.py
@@ -0,0 +1,20 @@
+""" 
+Flag (Bandeira) - Marcar um local
+None = Não valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = Identidade
+
+"""
+
+# As duas variáveis apontam para o mesmo valor na memória
+v1 = 'a'
+v2 = 'a'
+print(id(v1))
+print(id(v2))
+
+# condicao = False
+
+# if condicao:
+#   print("Faça algo")
+# else:
+#   print("Não faça algo")
