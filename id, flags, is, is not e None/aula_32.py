a5850d563a57521601580eab7b0c5affaf40e113
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-12 07:03:49 -0300

    aula 32

--- id, flags, is, is not e None/aula_32.py
+++ id, flags, is, is not e None/aula_32.py
@@ -53,4 +53,27 @@ Exe 3:
 Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
+
+nome = input("Digite seu primeiro nome: ")
+
+try:
+    nome = int(nome)
+    print("Isso não é um nome, é um número")
+except ValueError:
+    try:
+        nome = float(nome)
+        print("Isso não é um nome, é um número flutuante")
+    except ValueError:
+        nome = str(nome).strip()
+        tamanho_nome = len(nome)
+        
+        if tamanho_nome == 0:
+            print("Você não digitou nada")
+        elif 1 <= tamanho_nome <= 4:
+            print("Seu nome é curto")
+        elif 5 <= tamanho_nome <= 6:
+            print("Seu nome é normal")
+        else:
+            print("Seu nome é muito grande")
+
 """
