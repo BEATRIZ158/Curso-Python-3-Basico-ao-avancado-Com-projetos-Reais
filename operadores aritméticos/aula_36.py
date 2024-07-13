9a6884088af599df78b8a7f7a44a63a491e89b3d
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-13 07:25:37 -0300

    aula 36

---
+++ operadores aritméticos/aula_36.py
@@ -0,0 +1,22 @@
+""" 
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+
+contador = 0
+
+#while contador <= 10:
+#    contador += 1
+#    print(contador)
+
+contador = 10
+
+###
+
+#contador %= 2
+#print(contador)
+
+# também
+
+contador /= 5
+print(contador)
