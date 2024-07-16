bf63c18e6b40487baaaa025ee9308365efc7496f
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-15 23:24:14 -0300

    aula 47

--- estruturas de repetição/aula_47.py
+++ estruturas de repetição/aula_47.py
@@ -14,3 +14,26 @@ na palavra secreta.
 Faça a contagem de tentativas do seu
 usuário.
 """
+
+palavra_chave = 'abacate'
+cont = 0
+palavra_formatada = list('*******')
+
+while True:
+    letra = input("Digite uma letra: ").lower()[0]
+    
+    if letra in palavra_chave:
+        for i, letra_chave in enumerate(palavra_chave):
+            if letra_chave == letra:
+                palavra_formatada[i] = letra
+                cont += 1
+            palavra_formatada_str = ''.join(palavra_formatada) 
+            if palavra_chave == palavra_formatada_str:
+                print(f"Você venceu com {cont} tentativas")
+                break
+    else:
+        cont += 1
+    print(f"Palavra formatada: {palavra_formatada_str}")
+
+# Método join é usado para concatenar ou juntar os elementos de uma lista (ou qualquer iterável) em uma única string. 
+# Ele é particularmente útil quando você tem uma lista de strings e deseja combiná-las em uma string única
