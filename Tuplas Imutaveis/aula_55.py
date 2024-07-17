476f1f7bb0bbbf8acedeb4a3e42fe13fd361e283
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-17 19:59:49 -0300

    Tuplas

---
+++ Tuplas Imutaveis/aula_55.py
@@ -0,0 +1,8 @@
+"""
+Tipo tupla - Uma lista imutável
+"""
+nomes = ('Maria', 'Helena', 'Luiz')
+# nomes = tuple(nomes) Converter Lista para Tupla
+# nomes = list(nomes) Converter Tupla para Lista
+print(nomes[-1])
+print(nomes)
