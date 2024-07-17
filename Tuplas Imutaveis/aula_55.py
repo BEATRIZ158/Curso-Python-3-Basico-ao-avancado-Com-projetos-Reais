e5fa35676645a4db7b749f206abaff35629cc11b
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-17 20:11:34 -0300

    enumerate

--- Tuplas Imutaveis/aula_55.py
+++ Tuplas Imutaveis/aula_55.py
@@ -6,3 +6,25 @@ nomes = ('Maria', 'Helena', 'Luiz')
 # nomes = list(nomes) Converter Tupla para Lista
 print(nomes[-1])
 print(nomes)
+
+"""
+enumerate - enumera iteráveis (índices)
+"""
+# [(0, 'Maria'), (1, 'Helena'), (2, 'Luiz'), (3, 'João')]
+lista = ['Maria', 'Helena', 'Luiz']
+lista.append('João')
+
+lista_enumerada = enumerate(lista)
+print(next(lista_enumerada))
+
+#for indice, nome in enumerate(lista):
+#    print(indice, nome, lista[indice])
+
+#for item in enumerate(lista):
+#    indice, nome = item  #indice recebe 0, nome recebe 'Maria'
+#    print(indice, nome)
+
+#for tupla_enumerada in enumerate(lista):
+#    print('FOR da tupla:')
+#    for valor in tupla_enumerada:
+#        print(f'\t{valor}')
