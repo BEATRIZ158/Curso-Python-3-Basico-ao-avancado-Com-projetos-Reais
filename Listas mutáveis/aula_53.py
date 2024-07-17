eeeef0a3dc55f65bddde53db1ed95a5517e904ee
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-17 18:04:39 -0300

    Aula 53

--- Listas mutáveis/aula_53.py
+++ Listas mutáveis/aula_53.py
@@ -10,3 +10,10 @@ lista = ['Maria', 'Helena', 'Luiz']
 
 for ind, nome in enumerate(lista):
     print(f"O índice {ind} tem o nome {nome}")
+
+# Ou
+
+indices = range(len(lista))
+
+for indice in indices:
+    print(f"O índice {indice} tem o nome {lista[indice]}")
