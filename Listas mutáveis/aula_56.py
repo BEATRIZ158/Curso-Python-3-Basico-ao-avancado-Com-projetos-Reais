311bca11ed9ef251058c4f41af20d6dd8095f72a
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-17 21:16:42 -0300

    Aula 56

--- Listas mutáveis/aula_56.py
+++ Listas mutáveis/aula_56.py
@@ -66,3 +66,44 @@ while True:
                 break
             else:
                 print("Opçõa errada!")
+
+# Correção do Professor
+
+"""
+import os
+
+lista = []
+
+while True:
+    print('Selecione uma opção')
+    opcao = input('[i]nserir [a]pagar [l]istar: ')
+
+    if opcao == 'i':
+        os.system('clear')
+        valor = input('Valor: ')
+        lista.append(valor)
+    elif opcao == 'a':
+        indice_str = input(
+            'Escolha o índice para apagar: '
+        )
+
+        try:
+            indice = int(indice_str)
+            del lista[indice]
+        except ValueError:
+            print('Por favor digite número int.')
+        except IndexError:
+            print('Índice não existe na lista')
+        except Exception:
+            print('Erro desconhecido')
+    elif opcao == 'l':
+        os.system('clear')
+
+        if len(lista) == 0:
+            print('Nada para listar')
+
+        for i, valor in enumerate(lista):
+            print(i, valor)
+    else:
+        print('Por favor, escolha i, a ou l.')
+"""
