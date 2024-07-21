457cd48e4a06167cc69aafcef8f4901cceec5612
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-21 14:55:43 -0300

    Código

--- Gerador de CPF/codigo.py
+++ Gerador de CPF/codigo.py
@@ -72,3 +72,35 @@ while True:
 
     except ValueError:
         print("A opção escolhida precisa ser um número INTEIRO! Tente novamente.")
+
+"""Solução do Professor
+
+# cpf = '36440847007'  # Esse CPF gera o primeiro dígito como 10 (0)
+cpf_enviado_usuario = '74682489070'
+nove_digitos = cpf_enviado_usuario[:9]
+contador_regressivo_1 = 10
+
+resultado_digito_1 = 0
+for digito in nove_digitos:
+    resultado_digito_1 += int(digito) * contador_regressivo_1
+    contador_regressivo_1 -= 1
+digito_1 = (resultado_digito_1 * 10) % 11
+digito_1 = digito_1 if digito_1 <= 9 else 0
+
+dez_digitos = nove_digitos + str(digito_1)
+contador_regressivo_2 = 11
+
+resultado_digito_2 = 0
+for digito in dez_digitos:
+    resultado_digito_2 += int(digito) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+digito_2 = (resultado_digito_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
+
+cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+
+if cpf_enviado_usuario == cpf_gerado_pelo_calculo:
+    print(f'{cpf_enviado_usuario} é válido')
+else:
+    print('CPF inválido')
+"""
