16c13c945265eb992126f9b0949c2c6e759736da
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-21 14:57:02 -0300

    Código

--- Gerador de CPF/codigo.py
+++ Gerador de CPF/codigo.py
@@ -76,7 +76,26 @@ while True:
 """Solução do Professor
 
 # cpf = '36440847007'  # Esse CPF gera o primeiro dígito como 10 (0)
-cpf_enviado_usuario = '74682489070'
+import re
+import sys
+
+# cpf_enviado_usuario = '746.824.890-70' \
+#     .replace('.', '') \
+#     .replace(' ', '') \
+#     .replace('-', '')
+entrada = input('CPF [746.824.890-70]: ')
+cpf_enviado_usuario = re.sub(
+    r'[^0-9]',
+    '',
+    entrada
+)
+
+entrada_e_sequencial = entrada == entrada[0] * len(entrada)
+
+if entrada_e_sequencial:
+    print('Você enviou dados sequenciais.')
+    sys.exit()
+
 nove_digitos = cpf_enviado_usuario[:9]
 contador_regressivo_1 = 10
 
