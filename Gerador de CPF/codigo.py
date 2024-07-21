d8d8425ebca367041498c0efb6f02ee80f5afbfb
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-20 21:54:48 -0300

    Código

--- Gerador de CPF/codigo.py
+++ Gerador de CPF/codigo.py
@@ -23,3 +23,45 @@ contrário disso:
 
 O primeiro dígito do CPF é 7
 """
+while True:
+    print("""{} GERADOR DE CPF {}
+[ 1 ] - Gerar CPF
+[ 2 ] - Sair
+""".format('*'*10, '*'*10))
+    
+    op = input("Sua opção?(1 ou 2)  ").replace(' ', '')
+    
+    try:
+        op = int(op)
+        
+        if op == 1:
+            while True:
+                valores = input("Digite seu CPF: ")
+                cpf = list(valores)
+            
+                if len(cpf) != 9:
+                    print("Você precisa passar 9 dígitos! Tente novamente.")
+                else:
+                    break
+                
+                try:
+                    cpf_numeros = [int(numero) for numero in cpf]
+                except ValueError:
+                    print("CPF deve conter apenas números.")
+                    cpf_numeros = []
+                if cpf_numeros:
+                    resultados = []
+                    for i, num in zip(range(10, 1, -1), cpf_numeros):
+                        resultado = i * num
+                    resultados.append(resultado)
+                    print(f"{num} X {i} = {resultado}")
+    
+                    print("Resultados das multiplicações:", resultados)
+        elif op == 2:
+            print("Você escolheu sair. Tchau, tchau...")
+            break
+        else:
+            print("Opção indisponível. Tente novamente!")
+
+    except ValueError:
+        print("A opção escolhida precisa ser um número INTEIRO! Tente novamente.")
