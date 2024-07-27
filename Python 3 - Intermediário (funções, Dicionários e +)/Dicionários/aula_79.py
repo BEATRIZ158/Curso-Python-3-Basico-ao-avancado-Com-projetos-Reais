536a6f6b9e5eec1f5bd05857fbaaab0923ab9120
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-27 20:59:20 -0300

    Shallow Copy vs Deep Copy

---
+++ Python 3 - Intermediário (funções, Dicionários e +)/Dicionários/aula_79.py
@@ -0,0 +1,26 @@
+# Métodos úteis dos dicionários em Python
+# len - quantas chaves
+# keys - iterável com as chaves
+# values - iterável com os valores
+# items - iterável com chaves e valores
+# setdefault - adiciona valor se a chave não existe
+# copy - retorna uma cópia rasa (shallow copy)
+# get - obtém uma chave
+# pop - Apaga um item com a chave especificada (del)
+# popitem - Apaga o último item adicionado
+# update - Atualiza um dicionário com outro
+import copy
+
+d1 = {
+    'c1': 1,
+    'c2': 2,
+    'l1': [0, 1, 2],
+}
+d2 = d1.copy() # O sinal = não copia, ele indica que d2 aponta para o mesmo dicionário que d1
+#d2 = copy.deepcopy(d1)
+
+d2['c1'] = 1000 # Altera c1 também
+d2['l1'][1] = 999999
+
+print(d1)
+print(d2)
