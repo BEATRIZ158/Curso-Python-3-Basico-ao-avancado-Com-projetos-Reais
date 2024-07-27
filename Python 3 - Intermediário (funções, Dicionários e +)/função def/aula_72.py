333aa742faf58d86c044f0eaf9dad1980af3e6b7
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-07-27 14:36:57 -0300

    Funções de primeira classe

--- Python 3 - Intermediário (funções, Dicionários e +)/função def/aula_72.py
+++ Python 3 - Intermediário (funções, Dicionários e +)/função def/aula_72.py
@@ -1,6 +1,11 @@
 """
 Higher Order Functions
 Funções de primeira classe
+
+Academicamente, os termos Higher Order Functions e First-Class Functions têm significados diferentes.
+
+Higher Order Functions - Funções que podem receber e/ou retornar outras funções
+First-Class Functions - Funções que são tratadas como outros tipos de dados comuns (strings, inteiros, etc...)
 """
 
 def saudacao(msg, nome):
