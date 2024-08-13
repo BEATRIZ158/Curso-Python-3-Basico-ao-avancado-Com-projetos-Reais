5047d1b50e7ec5dc124ec768fe320c9688427a31
BEATRIZ158 <beatrizmesquita158@gmail.com>
2024-08-13 15:08:27 -0300

    List Comprehension

--- Python 3 - Intermediário (funções, Dicionários e +)/List Comprehension/aula_86.py
+++ Python 3 - Intermediário (funções, Dicionários e +)/List Comprehension/aula_86.py
@@ -8,7 +8,7 @@ for numero in range(10):
 # print(lista)
 
 lista = [
-    numero * 2
-    for numero in range(10)
+    numero * 2 # Deste lado (Esquerdo) fica o valor que será incluído na lista
+    for numero in range(10) # Gera os valores
 ]
 print(lista)
